2be589a61ca37b56eb241ae8967ceaf06b348e88
ArMaVill <49162683+ArMaVill@users.noreply.github.com>
2019-09-16 12:24:21 -0600

    changing auth header name

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
-  const token = req.header('x-auth-token');
+  const token = req.header('Autorization');
 
   if (!token)
     return res.status(401).json({
